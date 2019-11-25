c542550e013ada86e5191b0fb99a6ba5b2494d8a
Marius <marius@example.com>
2019-11-25 11:20:13 +0100

    added picamera functionality to mqtt-sub program

--- mqtt/src/mqtt_sub.py
+++ mqtt/src/mqtt_sub.py
@@ -2,7 +2,10 @@ import paho.mqtt.client as mqtt
 import json
 import os
 import logging
+import sys
+from time import sleep
 from manager import Manager
+from picamera import PiCamera
 
 
 def on_connect(client, userdata, flags, rc):
@@ -28,15 +31,63 @@ def on_message(client, userdata, msg):
     logger.info("topic: " + msg.topic)
     logger.info("msg: " + str(msg.payload))
 
+    try:
+        dict = json.loads(msg.payload.decode('utf-8'))
+        for cmd in dict:
+            commands[dict[cmd]]()
+    except:
+        logger.error("bad command")
+        logger.error("sys_exc_info: " + sys.exc_info()[0])
+
+
+def capture():
+    logger.info("capture image")
+    camera.start_preview()
+
+    # camera warm-up time
+    sleep(2)
+    camera.capture(img_path)
+
+
+def video_start():
+    logger.info("start video recording")
+    camera.start_recording(vid_path)
+
+
+def video_stop():
+    logger.info("stop video recording")
+    camera.stop_recording()
+
+
+def delete():
+    logger.info("delete recorded files")
+    os.remove(img_path)
+    os.remove(vid_path)
+
 
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "DEBUG"))
 logger = logging.getLogger("[pi-camera]")
+
+# store absolute path
+app_path = os.path.abspath(os.getcwd())
+img_path = app_path + '/../image/shot.jpg'
+vid_path = app_path + '/../video/my_video.h264'
 
 # read config file
 config_mqtt = ''
 with Manager('config.json', 'r') as config_file:
     config_mqtt = json.load(config_file)['mqtt']
 logger.debug("config.json loaded")
+
+commands = {
+    'capture': capture,
+    'video_start': video_start,
+    'video_stop': video_stop,
+    'delete': delete
+}
+
+# make PiCamera instance
+camera = PiCamera(resolution=(1024, 768))
 
 # make mqqt client
 mqttc = mqtt.Client()
